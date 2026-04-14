326e7743a29305228d553855361ec572b264be3a
mphele <mphelemoswane3@gmail.com>
2026-04-14 15:34:02 +0200

    Fix: inline questioning finally working

--- sieve-backend/src/index.ts
+++ sieve-backend/src/index.ts
@@ -6,6 +6,7 @@ import jobRoutes from './routes/jobRoutes';
 import { apiKeyAuth } from './middleware/auth'; 
 import sessionRoutes from './routes/sessionRoutes';
 import { setupSwagger } from './config/swagger';
+import applicantRoutes from './routes/applicantRoutes';
 
 dotenv.config();
 
@@ -28,8 +29,12 @@ app.use('/api/jobs', apiKeyAuth, jobRoutes);
 // Protect all /api/sessions routes
 app.use('/api/sessions', apiKeyAuth, sessionRoutes);
 
+// Protect all /api/applicants routes
+app.use('/api/applicants', applicantRoutes);
+
 // Initialize Swagger UI
 setupSwagger(app);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
