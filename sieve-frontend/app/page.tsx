aaa85cac34ff0ec7fc1a0e197ac9de3ef3c90a65
Elvis Chege <96030189+elviscgn@users.noreply.github.com>
2026-04-17 06:13:23 +0200

    revert dashboard

--- sieve-frontend/app/page.tsx
+++ sieve-frontend/app/page.tsx
@@ -1,20 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/dashboard");
-  }, [router]);
-
-  return (
-    <div className="min-h-screen bg-[#f4f7fe] flex items-center justify-center">
-      <FontAwesomeIcon icon={faSpinner} spin className="text-4xl text-primary" />
-    </div>
-  );
+  redirect("/dashboard");
 }
