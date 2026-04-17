55d0b37060c8c69ecef9114051df60558ac5a408
mphele <mphelemoswane3@gmail.com>
2026-04-17 15:25:22 +0200

    utils cleaned up

--- sieve-backend/src/utils/eventEmitter.ts
+++ sieve-backend/src/utils/eventEmitter.ts
@@ -1,5 +1,6 @@
-import { EventEmitter } from 'events';
+import { EventEmitter } from "events";
 
+// Initializes and exports a singleton EventEmitter instance used to broadcast and listen to application-wide real-time events.
 const globalEmitter = new EventEmitter();
 
 export default globalEmitter;
